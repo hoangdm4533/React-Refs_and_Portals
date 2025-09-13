adc0cd136132d1f6b244e97f18492443fbef63a0
HoangDev <hoangdmse184533@fpt.edu.vn>
2025-09-13 09:37:08 +0700

    timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
-
+import TimerChallenge from './components/TimerChallenge.jsx';
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title="Easy Challenge" targetTime={1} />
+        <TimerChallenge title="Medium Challenge" targetTime={5} />
+        <TimerChallenge title="Hard Challenge" targetTime={10} />
+        <TimerChallenge title="Expert Challenge" targetTime={15} />
+      </div>
+
     </>
   );
 }
